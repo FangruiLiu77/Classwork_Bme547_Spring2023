829a007bb079e81137eac5f3a0e16865be57c260
Fangrui <fangrui.liu7@google.com>
2023-02-10 12:54:45 -0500

    Converted database to the doctionary

--- database.py
+++ database.py
@@ -1,51 +1,70 @@
 #database
 
-def create_patient_entry(patient_name, patient_mrn, patient_age):
-    new_patient = [patient_name, patient_mrn, patient_age,[]]
+def create_patient_entry(first_name, last_name, patient_mrn, patient_age):
+    #new_patient = [patient_name, patient_mrn, patient_age,[]]
+    new_patient = {"First Name": first_name, "Last Name": last_name,
+                   "MRN": patient_mrn, "Age": patient_age,
+                   "Tests": []}
     return new_patient
 
+def get_full_name(patient):
+    return "{} {}".format(patient["First Name"], patient["Last Name"])
+
+def print_database(db):
+    for patient in db.values():
+        print("MRN: {}, Full Name: {}, Age: {}".format(patient["MRN"], get_full_name(patient), patient["Age"]))
+
 def main_driver():
-    db= []
-    db.append(create_patient_entry("Ann Ables", 1, 34))
-    db.append(create_patient_entry("Bob Boyles", 2, 45))
-    db.append(create_patient_entry("Chris Chou", 3, 52))
+    db = {}
+    db[1] = (create_patient_entry("Ann", "Ables", 1, 34))
+    db[2] = (create_patient_entry("Bob",  "Boyles", 2, 45))
+    db[3] = (create_patient_entry("Chris", "Chou", 3, 52))
     print(db)
+    print_database(db)
     add_test_to_patient(db, 1, "HDL", 120)
     add_test_to_patient(db, 2, "LDL", 100)
     add_test_to_patient(db, 2, "HDL", 99)
-    room_numbers = ["103", "232", "333"]
-    print(db)
-    print_directory(db, room_numbers)
-    get_test_value(db, 2, "LDL")
+    print_database(db)
+    #room_numbers = ["103", "232", "333"]
+    #print(db)
+    #print_directory(db, room_numbers)
+    print(get_test_result(db, 2, "LDL"))
+    return 
+
 
 def print_directory(db, room_numbers):
     for i, patient in enumerate(db):
         print("Patient {} is in room {}".format(patient[0], room_numbers[i]))
-    for patient, rn in zip(db,room_numbers):
+    for patient, rn in zip(db, room_numbers):
         print("Patient {} is in room {}".format(patient[0], rn))
 
-def get_test_value(db, mrn_to_find, test_name):
-    for patient in db:
-        if patient[1] == mrn_to_find:
-            for test in patient[3]:
-                if test[0] == test_name:
-                    print("The {} value of {} is {}".format(test_name, patient[0], test[1]))
-                else:
-                    continue
+def get_patient_entry(db, mrn_to_find):
+    patient = db[mrn_to_find]
+    if patient is None:
+        return False
+    return patient
 
-
-def get_patient_entry(mrn_to_find, db):
-    for patient in db:
-        if patient[1] == mrn_to_find:
-            return patient
-    return False
 
 def add_test_to_patient(db, mrn_to_find, test_name, test_value):
-    patient = get_patient_entry(mrn_to_find, db)
-    if patient == False:
+    patient = get_patient_entry(db, mrn_to_find)
+    if patient is False:
         print("Bad entry")
     else:
-        patient[3].append([test_name, test_value])
+        patient["Tests"].append([test_name, test_value])
+    return
+
+
+def get_test_value_from_test_list(test_list, test_name):
+    for test in test_list:
+        if test[0] == test_name:
+            return test[1]
+    return False
+
+
+def get_test_result(db, mrn, test_name):
+    patient = get_patient_entry(db, mrn)
+    test_value = get_test_value_from_test_list(patient["Test"], test_name)
+    return test_value
 
 if __name__ == "__main__":
     main_driver()
