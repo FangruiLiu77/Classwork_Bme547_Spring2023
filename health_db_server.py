e69da89180040c2841d757eea40b4c8187856376
Fangrui <fangrui.liu7@google.com>
2023-03-03 15:11:55 -0500

    class work on Mar3

---
+++ health_db_server.py
@@ -0,0 +1,11 @@
+"""
+Database = Dictionary
+keys -> ids for the patients
+value: int
+
+{1: {"id": 1, "name": "David", "blood_type": "o+" },
+ 2: {"id": 1, "name": "David", "blood_type": "o+"},
+ 3: {"id": 1, "name": "David",
+    "blood_type": "o+", "tests": []}
+ }
+"""
