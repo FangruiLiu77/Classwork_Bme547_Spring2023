0be19f9ed6a888c727d456375fc0e0591dec0b37
Fangrui <fangrui.liu7@google.com>
2023-03-05 12:38:49 -0500

    expectations class exercise

---
+++ errors.py
@@ -0,0 +1,18 @@
+def add_two_numbers(a, b):
+    answer = a + b
+    
+    if type(a) != int or type(b) != int:
+        raise TypeError("Cannot send a string.")
+    if a < 0 or b < 0:
+        raise ValueError("You cannot send a negative"
+                         "number to this function.")
+    
+    return answer
+
+
+def main():
+    print(add_two_numbers(4, 5))
+
+
+if __name__ == "__main__":
+    main()
