ea15043dafaf9fdd295970feb7634c5b1b62d838
Fangrui <fangrui.liu7@google.com>
2023-03-08 12:06:44 -0500

    errors

--- errors.py
+++ errors.py
@@ -1,12 +1,12 @@
 def add_two_numbers(a, b):
     answer = a + b
-    
+
     if type(a) != int or type(b) != int:
         raise TypeError("Cannot send a string.")
     if a < 0 or b < 0:
         raise ValueError("You cannot send a negative"
                          "number to this function.")
-    
+
     return answer
 
 
